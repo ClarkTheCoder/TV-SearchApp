937aa638f35007584ba8b11f7f1020ad4a77d6ea
carson <carsonclark2009@gmail.com>
2021-07-21 10:30:27 -0300

    added search functionality and tvmaze api

--- js/app.js
+++ js/app.js
@@ -1,7 +1,31 @@
 const tvSearchForm = document.querySelector('#tv-search-form')
 
 // listen for submit even on form
-tvSearchForm.addEventListener('submit', function (e) {
+tvSearchForm.addEventListener('submit', async function (e) {
     e.preventDefault()
-    console.log(tvSearchForm.elements.query.value)
+    const searchResult = tvSearchForm.elements.query.value;
+    // returns object with data property (an array of tv objects)
+    const result = await axios.get(`https://api.tvmaze.com/search/shows?q=${searchResult}`)
+    // display data array which contains tv shows 
+    displayImages(result.data)
+    resetSearchForm()
+
 })
+
+const displayImages = shows => {
+    // iterate through shows and pull out images if they exist
+    for (let tvShow of shows) {
+        // take show (object) and target image properties 
+        if (tvShow.show.image) {
+            // create a new image element and set source equal to tv show image
+            const img = document.createElement('img')
+            const divShows = document.querySelector('#shows')
+            img.src = tvShow.show.image.medium
+            // add show to show list in body 
+            divShows.append(img)
+        }
+    }
+}
+
+// reset button after query
+const resetSearchForm = () => tvSearchForm.elements.query.value = ''
